a77afaba999d16d47fd6360074e3c5cc9dcdc0ac
Miguel L. Rodrigues <miguellukas52@gmail.com>
2021-07-25 20:25:57 -0300

    updates

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ for _ in range(4000):
 for i in range(len(inputs)):
     predict = network.predict(inputs[i]).get_value(0, 0)
 
-    print("Input = {} | Output = {} | Expected = {}".format(inputs[i], outputs[i], predict))
+    print("Input = {} | Output = {} | Expected = {}".format(inputs[i], predict, outputs[i]))
